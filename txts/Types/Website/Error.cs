cc021725ddd728687457765c3b0d618f82323722
koko <koko@drones.gay>
2023-08-24 17:59:37 -0400

    Implement error type

---
+++ txts/Types/Website/Error.cs
@@ -0,0 +1,13 @@
+﻿namespace txts.Types.Website;
+
+public class Error
+{
+    public Error(string title, string message)
+    {
+        Title = title;
+        Message = message;
+    }
+
+    public string Title { get; set; }
+    public string Message { get; set; }
+}
