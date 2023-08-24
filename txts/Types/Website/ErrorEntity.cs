24fef84d9662ae9c69c2d6ce30d1aa24f061eba9
koko <koko@drones.gay>
2023-08-24 19:41:28 -0400

    Implement error entity and partial and migrate existing page errors

---
+++ txts/Types/Website/ErrorEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace txts.Types.Website;
+
+public class ErrorEntity
+{
+    public ErrorEntity(string title, string message)
+    {
+        this.Title = title;
+        this.Message = message;
+    }
+
+    public string Title { get; set; }
+    public string Message { get; set; }
+}
