d838da5908e436127e454a2c6ea771ebf81a9113
koko <koko@drones.gay>
2023-08-25 06:48:41 -0400

    Redesign site success and error status messages (#9)
    * Implement error type
    
    * Implement error entity and partial and migrate existing page errors
    
    * Remove unnecessary if statement in EditPage
    
    * Rework message partial/entity, implement success messages
    
    * Tweak creation success message string
    
    * Implement helper methods to simplify message modal creation
    
    * Rename methods to CreateSuccess/CreateError for clarity

---
+++ txts/Types/Website/MessageEntity.cs
@@ -0,0 +1,24 @@
+﻿namespace txts.Types.Website;
+
+public class MessageEntity
+{
+    public MessageEntity(string title, string message, MessageType type)
+    {
+        this.Title = title;
+        this.Message = message;
+        this.Type = type;
+    }
+
+    public string Title { get; set; }
+    public string Message { get; set; }
+    public MessageType Type { get; set; }
+
+    public static MessageEntity CreateSuccess(string title, string message) => new(title, message, MessageType.Success);
+    public static MessageEntity CreateError(string title, string message) => new(title, message, MessageType.Error);
+}
+
+public enum MessageType
+{
+    Success,
+    Error,
+}
