13d315d972fe7e39fd97afec8049f06d17090b7f
koko <koko@drones.gay>
2023-08-24 20:24:02 -0400

    Rework message partial/entity, implement success messages

---
+++ txts/Types/Website/MessageEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace txts.Types.Website;
+
+public class MessageEntity
+{
+    public MessageEntity(string title, string message, MessageType type)
+    {
+        this.Title = title;
+        this.Message = message;
+        this.Type = type;
+    }
+
+    public string Title { get; set; }
+    public string Message { get; set; }
+    public MessageType Type { get; set; }
+}
+
+public enum MessageType
+{
+    Success,
+    Error,
+}
