45affe4280572287cf8885dd80ae2917ac427ed2
koko <koko@drones.gay>
2023-08-24 21:14:54 -0400

    Rename methods to CreateSuccess/CreateError for clarity

--- txts/Types/Website/MessageEntity.cs
+++ txts/Types/Website/MessageEntity.cs
@@ -13,8 +13,8 @@ public class MessageEntity
     public string Message { get; set; }
     public MessageType Type { get; set; }
 
-    public static MessageEntity Success(string title, string message) => new(title, message, MessageType.Success);
-    public static MessageEntity Error(string title, string message) => new(title, message, MessageType.Error);
+    public static MessageEntity CreateSuccess(string title, string message) => new(title, message, MessageType.Success);
+    public static MessageEntity CreateError(string title, string message) => new(title, message, MessageType.Error);
 }
 
 public enum MessageType
