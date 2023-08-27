8a7ddeb170975738f931c820a65b17f9d83a3655
koko <koko@drones.gay>
2023-08-27 13:35:13 -0400

    Add logging to updater script

--- txts-update.sh
+++ txts-update.sh
@@ -7,6 +7,11 @@ WORKING_DIRECTORY=/srv/txts # change this to match your deployment
 GIT_REMOTE=origin
 GIT_BRANCH=master
 
+# Helper functions
+function log() {
+	echo "[$(date)] $*"
+}
+
 # Check for root privileges.
 if [ "$EUID" -ne 0 ]
   then echo "This script needs to be run as root!"
@@ -14,13 +19,18 @@ if [ "$EUID" -ne 0 ]
 fi
 
 # Update the service.
+log "Working directory for updater script is $WORKING_DIRECTORY"
 cd $WORKING_DIRECTORY || exit 1
 
 # Pull the latest changes from the git repository.
+log "Git remote for updater script is $GIT_REMOTE"
+log "Git branch for updater script is $GIT_BRANCH"
 sudo git pull $GIT_REMOTE $GIT_BRANCH
 
 # Restart the service.
+log "Restarting the txts system service"
 sudo systemctl restart txts.service
 
 # Exit with a success code.
+log "Update completed"
 exit 0
