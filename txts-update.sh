4d62decc4da8acfc5229db97bba1c4245cd1bcda
koko <koko@drones.gay>
2023-08-27 13:13:07 -0400

    Add update script template

---
+++ txts-update.sh
@@ -0,0 +1,20 @@
+﻿#!/usr/bin/env bash
+
+# This script updates and redeploys the txts service.
+
+# Runtime variables
+WORKING_DIRECTORY=/srv/txts # change this to match your deployment
+GIT_REMOTE=origin
+GIT_BRANCH=master
+
+# Update the service.
+cd $WORKING_DIRECTORY || exit 1
+
+# Pull the latest changes from the git repository.
+sudo git pull $GIT_REMOTE $GIT_BRANCH
+
+# Restart the service.
+sudo systemctl restart txts.service
+
+# Exit with a success code.
+exit 0
