81e4b0e191527fc9e60e8b2a52bcec63c36d993f
koko <koko@drones.gay>
2023-08-27 13:13:56 -0400

    Add root check to update script

--- txts-update.sh
+++ txts-update.sh
@@ -7,6 +7,12 @@ WORKING_DIRECTORY=/srv/txts # change this to match your deployment
 GIT_REMOTE=origin
 GIT_BRANCH=master
 
+# Check for root privileges.
+if [ "$EUID" -ne 0 ]
+  then echo "This script needs to be run as root!"
+  exit 1
+fi
+
 # Update the service.
 cd $WORKING_DIRECTORY || exit 1
 
